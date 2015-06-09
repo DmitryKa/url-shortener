0ad303aaae03c529dca937b0c9dd2235fd16ebd5
Dmitry Kamorin <dmitry.kamorin@aldigit.com>
2015-06-09 14:50:00 +0600

    Fix validation

--- src/Ttb/ShortenerBundle/Controller/DefaultController.php
+++ src/Ttb/ShortenerBundle/Controller/DefaultController.php
@@ -4,19 +4,21 @@ namespace Ttb\ShortenerBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Ttb\Entity\Url;
 use Ttb\Form\Type\UrlType;
 
 class DefaultController extends Controller
 {
     public function indexAction(Request $request)
     {
-        $form = $this->createForm(new UrlType());
+        $url = new Url();
+        $form = $this->createForm(new UrlType(), $url);
 
         $form->handleRequest($request);
 
         if($form->isValid())
         {
-            var_dump('valid!'); die;
+            var_dump($url); die;
         }
         return $this->render('TtbShortenerBundle:Default:index.html.twig',
           array('form' => $form->createView()));
