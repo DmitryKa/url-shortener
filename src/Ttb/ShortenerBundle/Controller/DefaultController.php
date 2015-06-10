5d46f9fc57386903fb0e707d3931528197ec84d6
Dmitry Kamorin <dmitry.kamorin@aldigit.com>
2015-06-10 19:22:12 +0600

    Add url reducing

--- src/Ttb/ShortenerBundle/Controller/DefaultController.php
+++ src/Ttb/ShortenerBundle/Controller/DefaultController.php
@@ -9,32 +9,49 @@ use Ttb\Form\Type\UrlType;
 
 class DefaultController extends Controller
 {
+    const PREFIX_KEY = 'key:';
+    const PREFIX_HASH = 'hash:';
+    const PREFIX_CONFIG = 'config:';
+
     public function indexAction(Request $request)
     {
-        $url = new Url();
-        $form = $this->createForm(new UrlType(), $url);
+        $form = $this->createForm(new UrlType());
 
         $form->handleRequest($request);
 
         if($form->isValid())
         {
-            var_dump($url); die;
+            $full_url = $form->getData()['full_url'];
+            return $this->forward('TtbShortenerBundle:Default:shrink',
+              array('full_url' => $full_url));
         }
         return $this->render('TtbShortenerBundle:Default:index.html.twig',
           array('form' => $form->createView()));
     }
 
-    public function shrinkAction($url) {
-//        $redis = $this->container->get('snc_redis.default');
-//        $val = $redis->incr('foo:bar');
-//        $redis->set('my_key', 'my_val');
-//        $redis_cluster = $this->container->get('snc_redis.cluster');
-//        $val = $redis_cluster->get('ab:cd');
-//        $val = $redis_cluster->get('ef:gh');
-//        $val = $redis_cluster->get('ij:kl');
+    public function shrinkAction($full_url) {
+        $converter = $this->get('converter');
+        $redis = $this->container->get('snc_redis.default');
+
+        $hash = hash('sha256', $full_url);
+        $keys = $redis->smembers(self::PREFIX_HASH . $hash);
+        $created = false;
+
+        foreach($keys as $key) {
+            $full_url_existent = $redis->get(self::PREFIX_KEY . $key);
+            if ($full_url == $full_url_existent) {
+                $created = true;
+            }
+        }
+        if(!$created) {
+            $counter = $redis->incr(self::PREFIX_CONFIG . 'counter');
+            $key = $converter->base62_encode($counter);
+            $redis->set(self::PREFIX_KEY . $key, $full_url);
+            $redis->sadd(self::PREFIX_HASH . $hash, $key);
+        }
 
         return $this->render('TtbShortenerBundle:Default:shrink.html.twig',
-          array('url' => $url)
+          array('key' => $key)
         );
     }
 }
