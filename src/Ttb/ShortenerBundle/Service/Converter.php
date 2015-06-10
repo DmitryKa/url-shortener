ce197043d727d7a04cf1f5f1b622a6cebdff2926
Dmitry Kamorin <dmitry.kamorin@aldigit.com>
2015-06-10 19:19:41 +0600

    Add service for base62 encoding

---
+++ src/Ttb/ShortenerBundle/Service/Converter.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Ttb\ShortenerBundle\Service;
+
+class Converter {
+    private $base = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    public function base62_encode($num) {
+        $b = 62;
+        $r = $num  % $b ;
+        $res = $this->base[$r];
+        $q = floor($num/$b);
+        while ($q) {
+            $r = $q % $b;
+            $q =floor($q/$b);
+            $res = $this->base[$r].$res;
+        }
+        return $res;
+    }
+
+    public function base62_decode($num) {
+        $b = 62;
+        $limit = strlen($num);
+        $res=strpos($this->base,$num[0]);
+        for($i=1;$i<$limit;$i++) {
+            $res = $b * $res + strpos($this->base,$num[$i]);
+        }
+        return $res;
+    }
+}
