def1c5ee6629bb4b699779e12af7f01930042110
Dmitry Kamorin <dmitry.kamorin@aldigit.com>
2015-06-15 18:30:48 +0600

    Ref

--- src/Ttb/ShortenerBundle/Resources/public/js/main.js
+++ src/Ttb/ShortenerBundle/Resources/public/js/main.js
@@ -1,23 +1,12 @@
-//function validateUrl() {
-//    alert('tadam');
-//}
-//
-//document.addEventListener('DOMContentLoaded', function () {
-//    document.getElementById('url_form').addEventListener('submit', function() {
-//            alert('moahaha!');
-//            return false;
-//        }
-//    );
-//});
-
 function validateUrl() {
     var input = document.getElementById('url_full_url');
+    var urlRegex = /((https?)|(ftp)):\/\/(www\.)?\S+\.\S+/;
     if(!(input.value.indexOf('http://') === 0) &&
         !(input.value.indexOf('https://') === 0) &&
         !(input.value.indexOf('ftp://') === 0) ) {
         input.value = 'http://' + input.value;
     }
-    if(input.value.match(/((https?)|(ftp)):\/\/(www\.)?\S+\.\S+/)) {
+    if(input.value.match(urlRegex)) {
         return;
     }
     return false;
