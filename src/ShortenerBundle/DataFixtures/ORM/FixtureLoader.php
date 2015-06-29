c98e5f722b37495b385f90112edf8bc581dcdc47
Dmitry Kamorin <dkamorin@thumbtack.net>
2015-06-29 13:44:12 +0600

    Ref: cleaning

--- src/ShortenerBundle/DataFixtures/ORM/FixtureLoader.php
+++ src/ShortenerBundle/DataFixtures/ORM/FixtureLoader.php
@@ -13,27 +13,11 @@ use Symfony\Component\Security\Core\Encoder\MessageDigestPasswordEncoder;
 
 class FixtureLoader implements FixtureInterface {
     public function load(ObjectManager $manager) {
-////         создание роли ROLE_ADMIN
         $role = new Role();
         $role->setName('ROLE_AUTHENTICATED_USER');
 
         $manager->persist($role);
 
-//        // создание пользователя
-//        $user = new User();
-//        $user->setEmail('john@example.com');
-//        $user->setUsername('john');
-//        $user->setSalt(md5(time()));
-//
-//        // шифрует и устанавливает пароль для пользователя,
-//        // эти настройки совпадают с конфигурационными файлами
-//        $encoder = new MessageDigestPasswordEncoder('sha512', TRUE, 10);
-//        $password = $encoder->encodePassword('admin', $user->getSalt());
-//        $user->setPassword($password);
-//
-//        $user->getUserRoles()->add($role);
-//
-//        $manager->persist($user);
         $manager->flush();
     }
 }
